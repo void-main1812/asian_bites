d0bdd3d3df18f68d9b71b3506dce7d6bfe7be4f7
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-02-06 18:53:03 +0530

    ✨feat: Register screen UI

--- app/components/SocialAuth.tsx
+++ app/components/SocialAuth.tsx
@@ -17,13 +17,13 @@ const SocialAuth: React.FC<SocialAuthProps> = ({
 }) => {
   return (
     <View
-      className="w-full py-5 justify-center items-center px-6 mb-5 rounded-lg flex flex-row space-x-4 shadow-2xl"
+      className="w-full py-4 justify-center items-center px-6 mb-5 rounded-lg flex flex-row space-x-4 shadow-2xl"
       style={{
         backgroundColor: backgroundColor,
       }}
     >
       <Image source={image} className="h-6 w-6 aspect-square" />
-      <Text className={classname}>{text}</Text>
+      <Text className={`text-lg ${classname}`}>{text}</Text>
     </View>
   );
 };
